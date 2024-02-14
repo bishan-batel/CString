741c80977761788345245bb8d89533bcbb84968c
Kishan S Patel <bishan.batel@protonmail.com>
2024-02-14 15:45:52 -0800

    Updated README

--- utils.hpp
+++ utils.hpp
@@ -9,20 +9,70 @@
 
 #include "cstdint"
 
+/**
+ * \brief 32 Bit Floating Point Number
+ */
+using f32 = float;
+
+/**
+ * \brief 64 Bit Floating Point Number
+ */
+using f64 = long double;
+
+/**
+ * \brief Unsigned 8 Bit Integer (cannot be negative)
+ */
 using u8 = std::uint8_t;
+
+/**
+ * \brief Unsigned 32 Bit Integer (cannot be negative)
+ */
 using u32 = std::uint32_t;
+
+/**
+ * \brief Unsigned 64 Bit Integer (cannot be negative)
+ */
 using u64 = std::uint64_t;
+
+
+/**
+ * \brief Biggest Unsigned Integer type that the current platform can use (cannot be negative)
+ */
 using umax = std::uintmax_t;
+
+/**
+ * \brief Unsigned Integer for when referring to any form of memory size or offset (eg. an array length or index)
+ */
 using usize = std::size_t;
+
+/**
+ * \brief Unsigned Integer Pointer typically used for pointer arithmetic
+ */
 using uptr = std::uintptr_t;
 
+/**
+ * \brief Signed 8 bit Integer
+ */
 using i8 = std::int8_t;
-using i32 = std::int32_t;
-using i64 = std::int64_t;
-using imax = std::intmax_t;
-using iptr = std::intptr_t;
 
-using f32 = float;
-using f64 = long double;
+/**
+ * \brief Signed 32 bit Integer
+ */
+using i32 = std::int32_t;
+
+/**
+ * \brief Signed 64 bit Integer
+ */
+using i64 = std::int64_t;
+
+/**
+ * \brief Biggest Integer type that the current platform can use
+ */
+using imax = std::intmax_t;
+
+/**
+ * \brief Integer pointer typically used for pointer arithmetic
+ */
+using iptr = std::intptr_t;
 
 #endif //UTILS_HPP
